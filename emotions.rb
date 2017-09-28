4bf3733cdd71aa55e80417ad61115343b158bc36
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-28 19:11:20 -0400

    Display messages

--- emotions.rb
+++ emotions.rb
@@ -1,8 +1,35 @@
+class Person
 
-emotions = {}
+  def initialize(name)
+    @name = name
+    @emotions = {}
 
-emotions[:happines] = 3
-emotions[:anger] = 1
-emotions[:fear] = 2
-emotions[:joy] = 3
-emotions[:surprise] = 2
+    @emotions[:happines] = 3
+    @emotions[:anger] = 1
+    @emotions[:fear] = 2
+    @emotions[:joy] = 3
+    @emotions[:surprise] = 2
+  end
+
+  def emotions
+    @emotions
+  end
+
+  def message
+    @emotions.each do |key, value|
+      if value == 3
+      p "I am feeling a high amount of #{key}."
+      elsif value == 2
+      p "I am feeling a medium amount of #{key}."
+      elsif value == 1
+      p "I am feeling a low amount of #{key}."
+      end
+    end
+  end
+
+end
+
+me = Person.new("Tony")
+p me.emotions
+p "----------------"
+me.message
